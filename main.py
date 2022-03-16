6513618765a111950728d416b1ab6618193c58f5
nkryazhev <nkryazhev@gmail.com>
2022-03-16 19:24:00 +0300

    update candidate obj creation, fix templates

--- main.py
+++ main.py
@@ -1,41 +1,38 @@
 from flask import Flask, render_template
 import json
 
-app = Flask(__name__)
 
-
-
-
-class CandidateFactory():
+class CandidateFactory:
     candidates = {}
 
-    def get_candidates_from_json(self, filename):
+    def create_candidates_from_json(self, filename):
         with open(filename, 'r', encoding='utf-8') as file:
-            _temp = json.load(file)
+            for parsed_item in json.load(file):
+                _id = parsed_item['id']
+                _name = parsed_item['name']
+                _picture = parsed_item['picture']
+                _position = parsed_item['position']
+                _gender = parsed_item['gender']
+                _age = parsed_item['age']
+                _skills = parsed_item['skills']
 
-            for candi in _temp:
-                cand = Candidate(candi['id'], candi['name'], candi['picture'], candi['position'], candi['gender'], candi['age'], candi['skills'])
-                self.candidates[candi['id']] = cand
-
+                # Create candidate object and register it in dictionary
+                self.candidates[_id] = Candidate(_id, _name, _picture, _position, _gender, _age, _skills)
 
     def get_by_skill(self, skill_name):
 
-        candidate_slice = []
-        for id, candidate in self.candidates.items():
+        candidate_list = []
+        for candidate in self.candidates.values():
             if candidate.has_skill(skill_name):
-                candidate_slice.append(candidate)
+                candidate_list.append(candidate)
 
-        if len(candidate_slice) == 0:
+        if len(candidate_list) == 0:
             return None
         else:
-            return candidate_slice
+            return candidate_list
 
 
-
-
-
-
-class Candidate():
+class Candidate:
 
     def __init__(self, id, name, picture, position, gender, age, skills):
         self.id = id
@@ -44,41 +41,41 @@ class Candidate():
         self.position = position
         self.gender = gender
         self.age = age
-        self.skills = skills.split(', ')
-
-
+        self.skills = skills
+        self.skills_set = set(self.skills.split(', '))
 
     def has_skill(self, skill_name):
-        return skill_name in self.skills
+        return skill_name in self.skills_set
 
 
+candidateDB = CandidateFactory()
+candidateDB.create_candidates_from_json('candidates.json')
 
-factory = CandidateFactory()
-
-factory.get_candidates_from_json('candidates.json')
-
-print(factory.candidates)
-
-print(factory.get_by_skill('python'))
-
+app = Flask(__name__)
 
 
 @app.route("/")
 def page_index():
-    return render_template("index.html", candidates=factory.candidates)
+    return render_template("index.html", candidates=candidateDB.candidates)
+
 
 @app.route("/candidate/<int:uid>")
 def page_profile(uid):
-    return render_template("candidate.html", candidate=factory.candidates[uid])
+    if uid in candidateDB.candidates.keys():
+        candidate = candidateDB.candidates[uid]
+        return render_template("candidate.html", candidate=candidate)
+    else:
+        return render_template("not_found.html")
+
 
 @app.route("/skill/<skill_name>")
 def page_skill(skill_name):
-
-    skilled_candidates = factory.get_by_skill(skill_name)
+    skilled_candidates = candidateDB.get_by_skill(skill_name)
 
     if skilled_candidates is None:
         return render_template("not_found.html")
     else:
         return render_template("skill.html", candidates=skilled_candidates)
+
 
 app.run(debug=True)
